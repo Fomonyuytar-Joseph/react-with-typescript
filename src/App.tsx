6708db9cd6b0e129fd2a4f5331de4815d459000d
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-08-05 09:19:12 +0100

    add TodoList functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-
+import { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import TodoList from './components/TodoList';
+import {Todo} from './model'
 
-function App() {
+// let name:number;
+// name=4;
+
+const App: React.FC =()=> {
+  //specify state to be string 
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd=(e:React.FormEvent)=>{
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos ,{id:Date.now(), todo:todo , isDone:false}])
+      setTodo("")
+    }
+  }
+
+  console.log(todos)
+  
   return (
     <div className="App">
-      Hello world
+      <span className="heading"> TODO APP</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
